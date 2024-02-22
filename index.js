36dbdcc18f823447a6562b3e998b50668f7719d7
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-21 21:35:10 -0800

    c

--- index.js
+++ index.js
@@ -28,3 +28,8 @@ app.use("/api/songs", songsRouter);
 app.get("/", (req, res) => {
     res.send("Welcome to the Music Album API!");
   });
+  
+  // 404 Middleware
+app.use((req, res) => {
+    res.status(404).json({ error: "Resource Not Found" });
+  });
