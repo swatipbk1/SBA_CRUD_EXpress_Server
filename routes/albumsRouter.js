c357feff553a9bb9db25d64c05e20d2331d71e7d
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-23 18:03:56 -0800

    c

--- routes/albumsRouter.js
+++ routes/albumsRouter.js
@@ -2,3 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const albums = require("../data/albums");
+
+// Album routes
+
+router.route("/")
+  .get((req, res) => {
+    res.json(albums);
+  })
