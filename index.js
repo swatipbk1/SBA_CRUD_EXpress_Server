a7fa8d4b5431a3ec4e9752ba217d1869e24beeaa
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-24 13:29:14 -0800

    c

--- index.js
+++ index.js
@@ -14,28 +14,27 @@ app.use(bodyParser.json());
 
 // Logging Middleware
 app.use((req, res, next) => {
-    const time = new Date();
-    console.log(`Received a ${req.method} request to ${req.url} at ${time.toLocaleTimeString()}.`);
-    next();
-  });
+  const time = new Date();
+  console.log(`Received a ${req.method} request to ${req.url} at ${time.toLocaleTimeString()}.`);
+  next();
+});
 
-  // Routes
+// Routes
 app.use("/api/albums", albumsRouter);
 app.use("/api/artists", artistsRouter);
 app.use("/api/songs", songsRouter);
 
 // Default route
 app.get("/", (req, res) => {
-    res.send("Welcome to the Music Album API!");
-  });
-  
-  // 404 Middleware
-app.use((req, res) => {
-    res.status(404).json({ error: "Resource Not Found" });
-  });
+  res.send("Welcome to the Music Album API!");
+});
 
-  // Start the server
+// 404 Middleware
+app.use((req, res) => {
+  res.status(404).json({ error: "Resource Not Found" });
+});
+
+// Start the server
 app.listen(port, () => {
-    console.log(`Server listening on port: ${port}.`);
-  });
-  
+  console.log(`Server listening on port: ${port}.`);
+});
