79bc2dda285abf777a7c1b78091261c07ce5739a
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-24 11:40:51 -0800

    c

--- routes/songsRouter.js
+++ routes/songsRouter.js
@@ -2,3 +2,8 @@ const express = require("express");
 const router = express.Router();
 
 const songs = require("../data/songs");
+// Song routes
+
+router.route("/")
+  .get((req, res) => {
+    let filteredSongs = songs;
