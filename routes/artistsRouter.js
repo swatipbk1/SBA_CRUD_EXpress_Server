1792119b1b3b54a50a3b1af7bbe58cefec06ee7a
swatipbk1 <swati.pandey.ca@gmail.com>
2024-02-23 19:29:57 -0800

    c

--- routes/artistsRouter.js
+++ routes/artistsRouter.js
@@ -7,3 +7,9 @@ router.route("/")
   .get((req, res) => {
     res.json(artists);
   })
+  .post((req, res) => {
+    // Placeholder implementation for creating a new artist
+    const newArtist = req.body; // Assuming the request body contains artist data
+    artists.push(newArtist);
+    res.status(201).json(newArtist);
+  });
